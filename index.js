0c0c3730588809738d53a87dc1d414300d221b98
Felix Fischer <felix@subtract.de>
2014-10-01 10:53:45 +0200

    implement caching

--- index.js
+++ index.js
@@ -1,28 +1,111 @@
-"use strict";
+/*!
+ * uitmuntend
+ * Copyright(c) 2014 Felix Fischer <kontakt@felixfischer.com>
+ * MIT Licensed
+ */
 
-var http = require('follow-redirects').http
+
+"use strict"
+
+var _ = require('lodash')
+  , http = require('follow-redirects').http
+  , ds_cache = require('ds-cache')
   , cheerio = require('cheerio')
-  , iconv = require('iconv-lite');
+  , iconv = require('iconv-lite')
 
-var load = require('./load')
-  , parse = require('./parse');
+var cache = new ds_cache({
+  limit_bytes: '5M',
+  auto_save:  true
+})
 
 
-function search(arg, callback) {
-  if (typeof callback !== 'function')
-    return new Error('callback should be a function')
-  if (typeof arg !== 'string')
-    return callback(new Error('arg should be a string'))
+/**
+ * `search()` will query uitmuntend.de or use cached results
+ *
+ * @param {String} query
+ * @param {Function} callback
+ */
+
+function search(query, callback){
+  var date = new Date()
   var options = {
     host: 'www.uitmuntend.de',
-    path: '/search.html?search=' + escape(arg),
-    agent: false
+    path: '/search.html?search=' + escape(query),
+    agent: false,
+    info: {query: query, timestamp: date.toUTCString()}
   }
-  load(options, function(res){
-    callback(parse(res, options))
-  })
+  var cached = cache.get(query)
+  if (cached) callback(cached)
+  else
+    load(options, function(body){
+      var res = parse(body)
+      res.options = options
+      cache.set(query, res)
+      callback(res)
+    })
 }
 
-exports.load = load
-exports.parse = parse
+
+/**
+ * `load()` is a wrapper for http.get
+ *
+ * @param {Object} options
+ * @param {Function} callback
+ */
+
+function load(options, callback){
+  var req = http.get(options, function(res){
+    var bodyChunks = []
+    res.on('data', function(chunk) {
+      bodyChunks.push(chunk)
+    }).on('end', function() {
+      callback(Buffer.concat(bodyChunks))
+    })
+  })
+  req.on('error', function(e) {callback(e, options)})
+}
+
+
+/**
+ * `parse()` extracts results from html and returns a nice object
+ *
+ * example:
+ *
+ * { results: [ {nl: woord, de: Wort, type: 'direkte Treffer'}, ... ] }
+ *
+ * @param {String} body
+ * @returns {Object}
+ */
+
+function parse(body) {
+  var $ = cheerio.load(iconv.decode(body, 'iso-8859-1'))
+  var out = { results: [], translations: [] }
+  $('.t2 tbody tr').each(function(i, el){
+    var cells = $(this).children()
+    var rowType = _.pluck(cells, 'name').toString()
+    var heading = null
+    switch (rowType) {
+      case 'th': // section header
+        heading = $(cells).text()
+        break
+      case 'td,td': // actual result
+        out.results.push({
+          nl: $(cells[0]).text(),
+          de: $(cells[1]).text(),
+          type: heading
+        })
+        // for backwards compatibility
+        out.translations.push({
+          original: $(cells[0]).text(),
+          translation: $(cells[1]).text()
+        })
+        break
+    }
+  })
+  return out
+}
+
+// expose functions
 exports.search = exports.query = search
+exports.parse = parse
+exports.load = load
--- test/test.js
+++ test/test.js
@@ -1,70 +1,28 @@
 "use strict";
 
-var fs = require('fs')
-  , should = require('should')
-  , uitmuntend = require('../index.js');
+var should = require('should')
+  , fs = require('fs')
+  , uitmuntend = require('../index.js')
 
-var testFile = ''
+var cacheFile = './ds_cache.json'
 
-describe("load(path, callback)", function(){
-  // file loading test
-  it("should load a local file", function(done){
-    uitmuntend.load(__dirname + '/sample-results/suche.html', function(res){
-      res.should.not.be.empty
-      testFile = res
-      done()
-    })
-  })
-})
-
-describe("parse(arg)", function(){
-
-  it("parser should be backwards compatible", function(done){
-    var res = uitmuntend.parse(testFile)
-    res.should.be.an.Object.with.property('translations')
-    res.translations.should.be.an.Array.which.is.not.empty
-    res.translations[0].should.have.keys('original', 'translation')
-    res.translations[0]['original'].should.containEql('search')
-    res.translations[0]['translation'].should.containEql('Suche')
-    done()
-  })
-
-  it("parser should parse as expected", function(done){
-    var res = uitmuntend.parse(testFile)
-    res.should.be.an.Object.with.property('results')
-    res.results.should.be.an.Object.with.property('direkte Treffer')
-    var res = res.results['direkte Treffer']
-    res.should.be.an.Array
-    res[0].should.have.keys('nl', 'de')
-    res[0]['nl'].should.containEql('search')
-    res[0]['de'].should.containEql('Suche')
-    done()
-  })
-})
 
 describe("search(arg, callback)", function(){
 
-  // bad callback
-  it("bad callback should return an error", function(done){
-    uitmuntend.search().should.be.an.Error
-    done()
-  })
-
-  // bad search arg
-  it("bad arg should return and call back with an error", function(done){
-    uitmuntend.search({}, function(res){
-      res.should.be.an.Error
-      return res
-    }).should.be.an.Error
+  beforeEach(function(done){
+    if (fs.existsSync(cacheFile)) fs.unlinkSync(cacheFile)
     done()
   })
 
   // futile search
   it("arg='xpqyz' should call back with an empty result set", function(done){
+    if (fs.existsSync(cacheFile)) fs.unlinkSync('./ds_cache.json')
     return uitmuntend.search('xpqyz', function(res){
-      res.should.be.an.Object.with.property('translations');
-      res.translations.should.be.an.Array.which.is.empty;
-      done();
+      res.should.be.an.Object.with.property('translations')
+      res.translations.should.be.an.Array.which.is.empty
+      res.should.be.an.Object.with.property('results')
+      res.results.should.be.an.Array.which.is.empty
+      done()
     })
   })
 
@@ -74,17 +32,19 @@ describe("search(arg, callback)", function(){
     it(exp, function(done){
       return uitmuntend.search(input, function(res){
         res.should.be.an.Object.with.property('results')
-        res.results.should.be.an.Object.with.property('direkte Treffer')
-        var res = res.results['direkte Treffer']
-        res.should.be.an.Array
-        res[0].should.be.an.Object.with.keys('nl', 'de')
-        res[0].nl.should.containEql(expectRes1)
-        res[0].de.should.containEql(expectRes2)
+        res.results.should.be.an.Array
+        res.results[0].should.be.an.Object.with.keys('nl', 'de', 'type')
+        res.results[0].nl.should.containEql(expectRes1)
+        res.results[0].de.should.containEql(expectRes2)
         done()
       })
     })
   }
 
+  // searching for the first time, will need to fetch
+  trySearch('Gläser', 'potten', 'Glas')
+  trySearch('zoektochten', 'zoektocht', 'Suche')
+  // searching for the second time, expecting fast cached results
   trySearch('Gläser', 'potten', 'Glas')
   trySearch('zoektochten', 'zoektocht', 'Suche')
 
